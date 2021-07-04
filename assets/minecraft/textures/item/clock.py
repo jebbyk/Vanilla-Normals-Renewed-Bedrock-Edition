5cf2773d4ec8b9717a4c012e95921a45e1ab55fb
jebbyk <zaicev9797@gmail.com>
2021-07-04 16:48:50 +0600

    delete unused files

--- assets/minecraft/textures/item/clock.py
+++
@@ -1,7 +0,0 @@
-from shutil import copyfile
-
-for i in range(1, 64) :
-    if i<10 :
-        copyfile("clock_00_s.png", "clock_0"+str(i)+"_s.png")
-    else :
-        copyfile("clock_00_s.png", "clock_"+str(i)+"_s.png")
