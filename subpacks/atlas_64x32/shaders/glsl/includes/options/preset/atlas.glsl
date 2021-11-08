dcde82547531fc224e6a2f87a873e50c540848f0
jebbyk <zaicev9797@gmail.com>
2021-11-08 14:48:57 +0600

    change name of atlas dimensions option file

--- subpacks/atlas_64x32/shaders/glsl/includes/options/preset/atlas.glsl
+++ subpacks/atlas_64x32/shaders/glsl/includes/options/preset/atlas.glsl
@@ -1,4 +1,3 @@
 // this file is used to support dfifferent types fo texturepacks
 
 #define TEXTURE_ATLAS_DIMENSION vec2(64.0, 32.0) // number of rows and columns in texture atlas. most of the times it should not be changed
-#define PBR_FEATURE_ENABLED
