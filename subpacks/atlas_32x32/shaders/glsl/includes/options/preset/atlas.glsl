27fe3b19a1d04fec5903a7d72ba5c8803a210553
jebbyk <zaicev9797@gmail.com>
2022-01-15 02:27:02 +0600

    rm empty folders

--- subpacks/atlas_32x32/shaders/glsl/includes/options/preset/atlas.glsl
+++
@@ -1,6 +0,0 @@
-// this file is used to support dfifferent types fo texturepacks
-
-#define TEXTURE_ATLAS_DIMENSION vec2(32.0, 32.0) // number of rows and columns in texture atlas. most of the times it should not be changed
-#define TEXTURE_PADDING 1.0
-#define WATER_NORMAL_MAP_RESOLUTION 16.0
-#define NOISE_MAP_RESOLUTION 16.0
